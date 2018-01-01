a83d24a11def625ec5f0eefc345bed35b6aa7765
fan <fan@fandeMacBook-Pro.local>
2018-01-01 15:28:29 +0800

    修改获取文件的格式

--- generateFnt.py
+++ generateFnt.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os  
 import sys  
 import codecs  
+import chardet
 
-originUTF = open("string.txt")
+originUTF = open("string.txt","rb")
 str = originUTF.read()
-
+print chardet.detect(str) 
+str=str.decode('utf-16')
 for j in str:
   print ord(j)
-
 
 originUTF.close()
 
