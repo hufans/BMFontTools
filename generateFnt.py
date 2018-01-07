a1f9a23f54121d3206fd044be3a059bf5b846ae6
fan <fan@fandeMacBook-Pro.local>
2018-01-07 17:37:17 +0800

    完成bmfont Tools

--- generateFnt.py
+++ generateFnt.py
@@ -2,14 +2,43 @@
 import os  
 import sys  
 import codecs  
-import chardet
+#import chardet
+import shutil
 
-originUTF = open("string.txt","rb")
-str = originUTF.read()
-print chardet.detect(str) 
-str=str.decode('utf-16')
-for j in str:
-  print ord(j)
+def getUnicode(Line):
+  originUTF = open("string.txt","rb")
+  configFile = open('config.bmfc', 'a')
+  str = originUTF.read()
+  #print chardet.detect(str)
+  str=str.decode('utf-16')
+  index = 0
+  for j in str:
+    lineToWrite = Line[index] +"," + unicode(ord(j)) +",0,0,0" 
+    configFile.write(lineToWrite + '\n')
+    index += 1 
+  
+  originUTF.close()
+  configFile.close()
 
-originUTF.close()
+def getFilePathAndWirteTofile(picPath):
+    fileNameOfconfig = "config.bmfc"
+    shutil.copyfile("configModel.bmfc", fileNameOfconfig)
+    L = []
+    originUTF = open(fileNameOfconfig,"rb")
+    for root, dirs, files in os.walk(path):  
+        for file in files: 
+          L.append("icon=\"" + root+"/"+file + '\"')
+
+    getUnicode(L)
+
+def callBmFont():
+      with open("config.json",'r') as load_f:
+      load_dict = json.load(load_f)
+      exePath = load_dict.BMFont_PATH,
+      os.system(exePath)
+
+path = os.getcwd()+ "/pic"
+getFilePathAndWirteTofile(path)
+callBmFont()
+
 
